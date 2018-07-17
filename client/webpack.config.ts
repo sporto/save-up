70313ecf3e512e3a91600731f25f271181877f17
Sebastian Porto <s@porto5.com>
2018-07-17 21:54:42 +1000

    More webpack config

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,27 +1,35 @@
-const elmSource = __dirname;
+// const elmSource = __dirname;
 
-const config = {
+import * as path from "path"
+import * as webpack from "webpack"
+import * as AssetsPlugin from "assets-webpack-plugin"
+
+let assetsPluginInstance = new AssetsPlugin()
+
+const config: webpack.Configuration = {
     entry: {
         admin: "./src/admin.ts",
     },
     output: {
-        filename: "[name].js",
-        path: __dirname + "/../api/static/bundles",
+        filename: "[name]-[chunkhash].js",
+        path: path.resolve(__dirname, "../api/static/bundles"),
     },
     module: {
-        rules: [{
-            test: /\.elm$/,
-            exclude: [/elm-stuff/, /node_modules/],
-            use: {
-                loader: "elm-webpack-loader",
-                options: {
-                    // cwd: elmSource,
-                }
-            }
-        }]
+        rules: [
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: {
+                    loader: "elm-webpack-loader",
+                    options: {
+                        // cwd: elmSource,
+                    },
+                },
+            },
+        ],
     },
     mode: "development",
-};
+    plugins: [assetsPluginInstance],
+}
 
-
-module.exports = config;
+export default config
