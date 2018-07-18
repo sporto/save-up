270249db0fbf5cb8b3a4174f6d95e99dd389ff1b
Sebastian Porto <s@porto5.com>
2018-07-18 20:23:57 +1000

    Add webpack serve

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -3,20 +3,28 @@
 import * as path from "path"
 import * as webpack from "webpack"
 import * as AssetsPlugin from "assets-webpack-plugin"
+import * as merge from "webpack-merge"
 
-let bundleDir = path.join(__dirname, "../api", "static", "bundles")
+const DEVELOPMENT = "development"
+const PRODUCTION = "production"
+let TARGET = process.env.NODE_ENV || DEVELOPMENT
+
+let outputPath = path.join(__dirname, "../api", "static", "bundles")
 
 let assetsPluginInstance = new AssetsPlugin({
-    path: bundleDir,
+    path: outputPath,
 })
 
-const config: webpack.Configuration = {
+let publicPath = "/webpack/"
+
+let baseConfig: webpack.Configuration = {
     entry: {
         admin: "./src/admin.ts",
     },
     output: {
-        filename: "[name]-[chunkhash].js",
-        path: bundleDir,
+        filename: "[name].js",
+        path: outputPath,
+        publicPath: publicPath,
     },
     module: {
         rules: [
@@ -35,5 +43,23 @@ const config: webpack.Configuration = {
     mode: "development",
     plugins: [assetsPluginInstance],
 }
+
+let devConfig = {}
+
+let prodConfig: webpack.Configuration = {
+    mode: "production",
+    output: {
+        filename: "[name]-[hash].js",
+    }
+}
+
+let config = null
+
+if (TARGET === DEVELOPMENT) {
+    config = merge(baseConfig, devConfig)
+} else {
+    config = merge(baseConfig, prodConfig)
+}
+  
 
 export default config
