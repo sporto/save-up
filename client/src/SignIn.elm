e4ac83b10f09d2177ed51e17ccce7349f65409fc
Sebastian Porto <s@porto5.com>
2018-07-19 19:53:25 +1000

    Add sign in form

--- client/src/SignIn.elm
+++ client/src/SignIn.elm
@@ -2,8 +2,11 @@ module SignIn exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, type_)
+import Html.Events exposing (onClick, onSubmit)
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
 
 
 type alias Flags =
@@ -11,12 +14,16 @@ type alias Flags =
 
 
 type alias Model =
-    { count : Int }
+    { email : String
+    , password : String
+    }
 
 
 initialModel : Model
 initialModel =
-    { count = 0 }
+    { email = ""
+    , password = ""
+    }
 
 
 init flags =
@@ -24,18 +31,56 @@ init flags =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = ChangeEmail String
+    | ChangePassword String
+    | Submit
+    | SubmitResponse (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | count = model.count + 1 }, Cmd.none )
+        ChangeEmail email ->
+            ( { model | email = email }, Cmd.none )
 
-        Decrement ->
-            ( { model | count = model.count - 1 }, Cmd.none )
+        ChangePassword password ->
+            ( { model | password = password }, Cmd.none )
+
+        Submit ->
+            ( model
+            , Http.send
+                SubmitResponse
+                (request model)
+            )
+
+        -- TODO store token and redirect
+        SubmitResponse (Ok _) ->
+            ( model, Cmd.none )
+
+        -- TODO log the error
+        SubmitResponse (Err _) ->
+            ( model, Cmd.none )
+
+
+
+-- TODO use flags for api
+
+
+request model =
+    Http.post "http://localhost:4010/sign-in" (requestBody model) responseDecoder
+
+
+requestBody : Model -> Http.Body
+requestBody model =
+    Encode.object
+        [ ( "email", Encode.string model.email )
+        , ( "password", Encode.string model.password )
+        ]
+        |> Http.jsonBody
+
+
+responseDecoder =
+    (Decode.string)
 
 
 subscriptions model =
@@ -52,6 +97,11 @@ main =
         }
 
 
+
+-- VIEW
+-- TODO add html validation
+-- TODO show errors
+
 view : Model -> Browser.Document Msg
 view model =
     { title = "KIC Admin"
@@ -61,8 +111,8 @@ view model =
                 [ h1 []
                     [ text "Sign In" ]
                 , form
-                    [ class "bg-white shadow-md rounded p-8 mt-3" ]
-                    [ p [  ]
+                    [ class "bg-white shadow-md rounded p-8 mt-3", onSubmit Submit ]
+                    [ p []
                         [ label [ class labelClasses ]
                             [ text "Email"
                             ]
@@ -72,7 +122,7 @@ view model =
                         [ label [ class labelClasses ]
                             [ text "Password"
                             ]
-                        , input [ class inputClasses ] []
+                        , input [ class inputClasses, type_ "password" ] []
                         ]
                     , p [ class "mt-6" ]
                         [ button [ class btnClasses ] [ text "Sign In" ]
