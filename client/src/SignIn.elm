d9d96321b8433856551cec9f8089efdf20adcb5c
Sebastian Porto <s@porto5.com>
2018-07-19 19:11:11 +1000

    Add sign in form

--- client/src/SignIn.elm
+++ client/src/SignIn.elm
@@ -1,8 +1,9 @@
 module SignIn exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (..)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
 
 
 type alias Flags =
@@ -41,17 +42,6 @@ subscriptions model =
     Sub.none
 
 
-view : Model -> Browser.Document Msg
-view model =
-    { title = "KIC Admin"
-    , body =
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
-        ]
-    }
-
-
 main : Program Flags Model Msg
 main =
     Browser.document
@@ -60,3 +50,47 @@ main =
         , update = update
         , view = view
         }
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "KIC Admin"
+    , body =
+        [ div [ class "flex items-center justify-center pt-16" ]
+            [ div []
+                [ h1 []
+                    [ text "Sign In" ]
+                , form
+                    [ class "bg-white shadow-md rounded p-8 mt-3" ]
+                    [ p [  ]
+                        [ label [ class labelClasses ]
+                            [ text "Email"
+                            ]
+                        , input [ class inputClasses ] []
+                        ]
+                    , p [ class "mt-6" ]
+                        [ label [ class labelClasses ]
+                            [ text "Password"
+                            ]
+                        , input [ class inputClasses ] []
+                        ]
+                    , p [ class "mt-6" ]
+                        [ button [ class btnClasses ] [ text "Sign In" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    }
+
+
+labelClasses =
+    "blocktext-sm font-bold"
+
+
+inputClasses =
+    "appearance-none border w-full py-2 px-3 text-grey-darker leading-tight mt-1"
+
+
+btnClasses =
+    "bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
