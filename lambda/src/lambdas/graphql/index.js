74a81879d73a1daa4a317f130b43941adc3e276c
Sebastian Porto <s@porto5.com>
2018-08-02 21:32:54 +1000

    WIP TS

--- lambda/src/lambdas/graphql/index.js
+++ lambda/src/lambdas/graphql/index.js
@@ -1,28 +1,21 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
 };
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-console.log(server)
-
+var _a = require('apollo-server-lambda'), ApolloServer = _a.ApolloServer, gql = _a.gql;
+// Construct a schema, using GraphQL schema language
+var typeDefs = gql(__makeTemplateObject(["\n  type Query {\n    hello: String\n  }\n"], ["\n  type Query {\n    hello: String\n  }\n"]));
+// Provide resolver functions for your schema fields
+var resolvers = {
+    Query: {
+        hello: function () { return 'Hello world!'; }
+    }
+};
+var server = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
+console.log(server);
 exports.handler = server.createHandler();
-
 // exports.handler = (event, context, callback) => {
 //   console.log('Handler start')
-
 //   callback(null, {
 //       statusCode: 200,
 //       body: new Date()
