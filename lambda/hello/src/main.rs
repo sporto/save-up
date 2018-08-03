4c1adbc29f16523b07145ea8248821ab898d3f5e
Sebastian Porto <s@porto5.com>
2018-08-03 11:31:41 +1000

    Fix example by returning body

--- lambda/hello/src/main.rs
+++ lambda/hello/src/main.rs
@@ -1,6 +1,20 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
 extern crate aws_lambda as lambda;
+
+#[derive(Serialize)]
+struct Response {
+    body: String,
+}
 
 fn main() {
     // start the runtime, and return a greeting every time we are invoked
-    lambda::start(|()| Ok("Hello ƛ!"))
+    lambda::start(|()| 
+        Ok(Response {
+            body: "Hello".to_owned(),
+        })
+    )
 }
