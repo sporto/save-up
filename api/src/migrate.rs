801be02c60e0626f7ec87bb7d5f3a78ba9011bdf
Sebastian Porto <s@porto5.com>
2018-08-14 16:45:28 +1000

    Add code for migrate

--- api/src/migrate.rs
+++ api/src/migrate.rs
@@ -1,36 +1,48 @@
 #[macro_use]
 extern crate diesel;
 #[macro_use]
+extern crate diesel_migrations;
+#[macro_use]
 extern crate failure;
-#[macro_use]
-extern crate juniper;
-#[macro_use]
-extern crate serde_derive;
-#[macro_use]
-extern crate serde_json;
-#[macro_use]
-extern crate validator_derive;
 
 extern crate aws_lambda as lambda;
-extern crate bcrypt;
-extern crate chrono;
-extern crate chrono_tz;
-extern crate jsonwebtoken as jwt;
-extern crate serde;
-extern crate uuid;
-extern crate validator;
 
-use lambda::event::apigw::{ApiGatewayProxyRequest};
-
+use failure::Error;
+use lambda::event::apigw::{ApiGatewayProxyRequest,ApiGatewayProxyResponse};
 use std::collections::HashMap;
 
 mod db;
-mod models;
-mod services;
 mod utils;
+
+embed_migrations!();
 
 fn main() {
 	lambda::start(|request: ApiGatewayProxyRequest| {
-		Ok("FO")
+
+		match run() {
+			Ok(_) =>
+				Ok(ApiGatewayProxyResponse {
+					body: None,
+					status_code: 200,
+					headers: HashMap::new(),
+					is_base64_encoded: None,
+				}),
+			Err(e) =>
+				Ok(ApiGatewayProxyResponse {
+					body: Some(e.to_string()),
+					status_code: 500,
+					headers: HashMap::new(),
+					is_base64_encoded: None,
+				}),
+		}
+
 	})
+}
+
+fn run() -> Result<(), Error> {
+	let connection = db::establish_connection()?;
+
+	embedded_migrations
+		::run_with_output(&connection, &mut std::io::stdout())
+		.map_err(|e| format_err!("{}", e.to_string()) )
 }
