2b5d148480945f1b622550118a7435895f9f6aac
Sebastian Porto <s@porto5.com>
2018-08-16 07:13:08 +1000

    Add example nunjucks render

--- api/mail/src/app.ts
+++ api/mail/src/app.ts
@@ -1,24 +1,47 @@
 import * as mjml2html from "mjml"
+import * as nunjucks from "nunjucks"
+
+let data = {
+	name: "Sam",
+}
+
+var template  = `
+<mjml>
+	<mj-body>
+		<mj-section>
+			<mj-column>
+				<mj-text>
+					Hello {{name}}!
+				</mj-text>
+			</mj-column>
+		</mj-section>
+	</mj-body>
+</mjml>
+`
+
+var res = nunjucks.renderString(template, data);
+
+console.log(res)
 
 /*
   Compile an mjml string
 */
 const htmlOutput = mjml2html(`
-  <mjml>
-    <mj-body>
-      <mj-section>
-        <mj-column>
-          <mj-text>
-            Hello World!
-          </mj-text>
-        </mj-column>
-      </mj-section>
-    </mj-body>
-  </mjml>
+	<mjml>
+		<mj-body>
+			<mj-section>
+				<mj-column>
+					<mj-text>
+						Hello {{name}}!
+					</mj-text>
+				</mj-column>
+			</mj-section>
+		</mj-body>
+	</mjml>
 `, {})
 
 
 /*
   Print the responsive HTML generated and MJML errors if any
 */
-console.log(htmlOutput)
+// console.log(htmlOutput)
