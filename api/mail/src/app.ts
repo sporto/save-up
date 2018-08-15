b7db35e6931c1ab6acaaa167c7be005e1ea54e4b
Sebastian Porto <s@porto5.com>
2018-08-15 12:51:42 +1000

    Add mjml example

---
+++ api/mail/src/app.ts
@@ -0,0 +1,24 @@
+import * as mjml2html from "mjml"
+
+/*
+  Compile an mjml string
+*/
+const htmlOutput = mjml2html(`
+  <mjml>
+    <mj-body>
+      <mj-section>
+        <mj-column>
+          <mj-text>
+            Hello World!
+          </mj-text>
+        </mj-column>
+      </mj-section>
+    </mj-body>
+  </mjml>
+`, {})
+
+
+/*
+  Print the responsive HTML generated and MJML errors if any
+*/
+console.log(htmlOutput)
