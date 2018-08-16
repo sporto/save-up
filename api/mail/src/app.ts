32eaf77e3756ba579e8610ab7f0b5ec8f3ea18b4
Sebastian Porto <s@porto5.com>
2018-08-16 17:27:52 +1000

    Add mail lambda

--- api/mail/src/app.ts
+++ api/mail/src/app.ts
@@ -3,48 +3,14 @@ import * as mjml2html from "mjml"
 import * as nunjucks from "nunjucks"
 import * as path from "path"
 
-let data = {
-	name: "Sam",
+exports.lambda_handler = async (event: AWSLambda.APIGatewayEvent, context: AWSLambda.Context, callback: AWSLambda.Callback) => {
+	let data = {
+		name: "Sam",
+	}
+	
+	let tem = path.join(__dirname, "templates", "invite.mjml")
+	let res = nunjucks.render(tem, data)
+	let compiled = mjml2html(res, {})
+
+	callback(null, compiled)
 }
-
-var template  = `
-<mjml>
-	<mj-body>
-		<mj-section>
-			<mj-column>
-				<mj-text>
-					Hello {{name}}!
-				</mj-text>
-			</mj-column>
-		</mj-section>
-	</mj-body>
-</mjml>
-`
-
-let tem = path.join(__dirname, "templates", "invite.mjml")
-var res = nunjucks.render(tem, data);
-
-console.log(res)
-
-/*
-  Compile an mjml string
-*/
-const htmlOutput = mjml2html(`
-	<mjml>
-		<mj-body>
-			<mj-section>
-				<mj-column>
-					<mj-text>
-						Hello {{name}}!
-					</mj-text>
-				</mj-column>
-			</mj-section>
-		</mj-body>
-	</mjml>
-`, {})
-
-
-/*
-  Print the responsive HTML generated and MJML errors if any
-*/
-// console.log(htmlOutput)
