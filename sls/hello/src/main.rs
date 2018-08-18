67387300c93687e4dfb72745973611eeecbe94df
Sebastian Porto <s@porto5.com>
2018-08-18 14:14:55 +1000

    Add commands to try

--- sls/hello/src/main.rs
+++ sls/hello/src/main.rs
@@ -5,11 +5,21 @@ use std::collections::HashMap;
 
 fn main() {
 	lambda::start(|request: ApiGatewayProxyRequest| {
+		let body = "Hello".to_owned();
+		
+		let mut headers = HashMap::new();
+
+		headers
+			.insert(
+				"Content-Type".to_owned(),
+				"application/json".to_owned()
+			); 
+
 		Ok(
 			ApiGatewayProxyResponse {
-				body: Some("Hello".to_owned()),
+				body: Some(body),
 				status_code: 200,
-				headers: HashMap::new(),
+				headers: headers,
 				is_base64_encoded: None,
 			}
 		)
