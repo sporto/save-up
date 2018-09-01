9f286cb63541c3165bbf4124da0b4514892af054
Sebastian Porto <s@porto5.com>
2018-09-01 12:51:13 +1000

    Use constants

--- api/graphql/src/models/role.rs
+++ api/graphql/src/models/role.rs
@@ -4,9 +4,8 @@ use diesel::serialize::{self, IsNull, Output, ToSql};
 use diesel::sql_types::*;
 use std::io;
 
-pub const ROLE_ADMIN: &str = "ADMIN";
-#[allow(dead_code)]
-pub const ROLE_INVESTOR: &str = "INVESTOR";
+pub const ADMIN: &[u8] = b"ADMIN";
+pub const INVESTOR: &[u8] = b"INVESTOR";
 
 #[derive(Debug, Copy, Clone, FromSqlRow, AsExpression, GraphQLEnum, PartialEq, Deserialize, Serialize)]
 #[sql_type = "Varchar"]
@@ -18,8 +17,8 @@ pub enum Role {
 impl ToSql<Text, Pg> for Role {
 	fn to_sql<W: io::Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
 		let _v = match *self {
-			Role::Admin => out.write_all(b"DEPOSIT")?,
-			Role::Investor => out.write_all(b"WITHDRAWAL")?,
+			Role::Admin => out.write_all(ADMIN)?,
+			Role::Investor => out.write_all(INVESTOR)?,
 		};
 		Ok(IsNull::No)
 	}
@@ -28,8 +27,8 @@ impl ToSql<Text, Pg> for Role {
 impl FromSql<Text, Pg> for Role {
 	fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
 		match not_none!(bytes) {
-			b"DEPOSIT" => Ok(Role::Admin),
-			b"WITHDRAWAL" => Ok(Role::Investor),
+			ADMIN => Ok(Role::Admin),
+			INVESTOR => Ok(Role::Investor),
 			_ => Err("Unrecognized enum variant".into()),
 		}
 	}
--- api/graphql/src/models/transaction_kind.rs
+++ api/graphql/src/models/transaction_kind.rs
@@ -4,6 +4,9 @@ use diesel::serialize::{self, IsNull, Output, ToSql};
 use diesel::sql_types::*;
 use std::io;
 
+pub const DEPOSIT: &[u8] = b"DEPOSIT";
+pub const WITHDRAWAL: &[u8] = b"WITHDRAWAL";
+pub const INTEREST: &[u8] = b"INTEREST";
 
 #[derive(Debug, Copy, Clone, FromSqlRow, AsExpression, GraphQLEnum, PartialEq)]
 #[sql_type = "Varchar"]
@@ -16,9 +19,9 @@ pub enum TransactionKind {
 impl ToSql<Text, Pg> for TransactionKind {
 	fn to_sql<W: io::Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
 		let _v = match *self {
-			TransactionKind::Deposit => out.write_all(b"DEPOSIT")?,
-			TransactionKind::Withdrawal => out.write_all(b"WITHDRAWAL")?,
-			TransactionKind::Interest => out.write_all(b"INTEREST")?,
+			TransactionKind::Deposit => out.write_all(DEPOSIT)?,
+			TransactionKind::Withdrawal => out.write_all(WITHDRAWAL)?,
+			TransactionKind::Interest => out.write_all(INTEREST)?,
 		};
 		Ok(IsNull::No)
 	}
@@ -27,9 +30,9 @@ impl ToSql<Text, Pg> for TransactionKind {
 impl FromSql<Text, Pg> for TransactionKind {
 	fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
 		match not_none!(bytes) {
-			b"DEPOSIT" => Ok(TransactionKind::Deposit),
-			b"WITHDRAWAL" => Ok(TransactionKind::Withdrawal),
-			b"INTEREST" => Ok(TransactionKind::Interest),
+			DEPOSIT => Ok(TransactionKind::Deposit),
+			WITHDRAWAL => Ok(TransactionKind::Withdrawal),
+			INTEREST => Ok(TransactionKind::Interest),
 			_ => Err("Unrecognized enum variant".into()),
 		}
 	}
