ca3df39203ab23b7cab980a7ebbc9d0f42e6f9fa
Sebastian Porto <s@porto5.com>
2018-09-09 00:30:13 +1000

    Get transactions

--- api/graphql/src/graph_app/queries/account.rs
+++ api/graphql/src/graph_app/queries/account.rs
@@ -18,13 +18,18 @@ graphql_object!(Account: AppContext |&self| {
 		BigDecimal::to_f64(&self.yearly_interest).unwrap()
 	}
 
-	field transactions(&executor, since: NaiveDateTime) -> Vec<Transaction> {
+	// since POSIX in milliseconds
+	// posix time should be kept in floats, because i32 will reset in the year 2038
+	field transactions(&executor, since: f64) -> Vec<Transaction> {
 		let ctx = &executor.context();
 		let conn = &ctx.conn;
 
+		let since_in_secs = since / 1000.0;
+		let since_dt = NaiveDateTime::from_timestamp(since_in_secs as i64, 0);
+
 		// We can access via account or accounts queries
 		// We assume that authorisation already happened in either of those
-		Transaction::find_by_account_id(conn, self.id, since)
+		Transaction::find_by_account_id(conn, self.id, since_dt)
 			.unwrap_or(vec![])
 	}
 });
