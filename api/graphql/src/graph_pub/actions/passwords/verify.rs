d708213d1bd9834cc9e89fd1b194b7bf0a074cc0
Sebastian Porto <s@porto5.com>
2018-09-14 23:54:38 +1000

    Update Juniper

--- api/graphql/src/graph_pub/actions/passwords/verify.rs
+++ api/graphql/src/graph_pub/actions/passwords/verify.rs
@@ -1,7 +1,7 @@
 use libreauth::pass::HashBuilder;
 
 pub fn call(password: &str, password_hash: &str) -> Result<bool, String> {
-	let checker = HashBuilder::from_phc(password_hash.to_string())
+	let checker = HashBuilder::from_phc(&password_hash.to_string())
 		.map_err(|_| "Failed to create checker".to_owned())?;
 
 	let is_valid = checker.is_valid(&password.to_owned());
