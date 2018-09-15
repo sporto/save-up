6b59451c7328215decc6e0d74fc78338839f7fe0
Sebastian Porto <s@porto5.com>
2018-09-15 21:25:17 +1000

    Delete old src

--- client/src-old/Shared/Context.elm
+++
@@ -1,8 +0,0 @@
-module Shared.Context exposing (Context)
-
-import Shared.Flags exposing (Flags, PublicFlags)
-
-
-type alias Context =
-    { flags : Flags
-    }
--- client/src-old/services/constants.ts
+++
@@ -1,2 +0,0 @@
-export const KIC_TOKEN = "kic-token"
-export const SIGN_IN_PATH = "/a/pub/sign-in"
