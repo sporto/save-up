602d0b569bee17c892a262c2ec58d7903e677990
Sebastian Porto <s@porto5.com>
2018-09-16 08:16:50 +1000

    Fix tests

---
+++ client/tests/RoutesTest.elm
@@ -0,0 +1,43 @@
+module RoutesTest exposing (suite)
+
+import Expect exposing (Expectation)
+import Shared.Routes exposing (..)
+import Test exposing (..)
+import Url exposing (Url)
+
+
+newUrl : Url
+newUrl =
+    { protocol = Url.Http
+    , host = ""
+    , port_ = Nothing
+    , path = "/app/admin/"
+    , query = Nothing
+    , fragment = Nothing
+    }
+
+
+parsingTest testCase path expected =
+    test testCase <|
+        \_ ->
+            let
+                url =
+                    { newUrl | path = path }
+
+                actual =
+                    parseUrl url
+            in
+            Expect.equal actual expected
+
+
+parsingTests : Test
+parsingTests =
+    describe "parse"
+        [ parsingTest "home" "/a/admin" routeForAdminHome
+        , parsingTest "home with trailing" "/a/admin/" routeForAdminHome
+        ]
+
+
+suite : Test
+suite =
+    describe "Routes" [ parsingTests ]
