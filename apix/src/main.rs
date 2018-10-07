63b5a7f9efd1caeac3622f4e69125aedb02faddb
Sebastian Porto <s@porto5.com>
2018-10-07 12:29:00 +1100

    Add Actix example

---
+++ apix/src/main.rs
@@ -0,0 +1,103 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate juniper;
+extern crate actix;
+extern crate actix_web;
+extern crate env_logger;
+extern crate futures;
+
+use actix::prelude::*;
+use actix_web::{
+	http, middleware, server, App, AsyncResponder, Error, FutureResponse, HttpRequest,
+	HttpResponse, Json, State,
+};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+use schema::{create_schema, Schema};
+
+mod schema;
+
+struct AppState {
+	executor: Addr<GraphQLExecutor>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GraphQLData(GraphQLRequest);
+
+impl Message for GraphQLData {
+	type Result = Result<String, Error>;
+}
+
+pub struct GraphQLExecutor {
+	schema: std::sync::Arc<Schema>,
+}
+
+impl GraphQLExecutor {
+	fn new(schema: std::sync::Arc<Schema>) -> GraphQLExecutor {
+		GraphQLExecutor { schema: schema }
+	}
+}
+
+impl Actor for GraphQLExecutor {
+	type Context = SyncContext<Self>;
+}
+
+impl Handler<GraphQLData> for GraphQLExecutor {
+	type Result = Result<String, Error>;
+
+	fn handle(&mut self, msg: GraphQLData, _: &mut Self::Context) -> Self::Result {
+		let res = msg.0.execute(&self.schema, &());
+		let res_text = serde_json::to_string(&res)?;
+		Ok(res_text)
+	}
+}
+
+fn graphiql(_req: &HttpRequest<AppState>) -> Result<HttpResponse, Error> {
+	let html = graphiql_source("http://127.0.0.1:8080/graphql");
+	Ok(HttpResponse::Ok()
+		.content_type("text/html; charset=utf-8")
+		.body(html))
+}
+
+fn graphql((st, data): (State<AppState>, Json<GraphQLData>)) -> FutureResponse<HttpResponse> {
+	st.executor
+		.send(data.0)
+		.from_err()
+		.and_then(|res| match res {
+			Ok(user) => Ok(HttpResponse::Ok()
+				.content_type("application/json")
+				.body(user)),
+			Err(_) => Ok(HttpResponse::InternalServerError().into()),
+		}).responder()
+}
+
+fn index(_req: &HttpRequest) -> &'static str {
+	"Hello world!"
+}
+
+fn main() {
+	::std::env::set_var("RUST_LOG", "actix_web=info");
+	env_logger::init();
+	let sys = actix::System::new("juniper-example");
+
+	let schema = std::sync::Arc::new(create_schema());
+	let addr = SyncArbiter::start(3, move || GraphQLExecutor::new(schema.clone()));
+
+	// Start http server
+	server::new(move || {
+		App::with_state(AppState{executor: addr.clone()})
+            // enable logger
+            .middleware(middleware::Logger::default())
+            .resource("/graphql", |r| r.method(http::Method::POST).with(graphql))
+            .resource("/graphiql", |r| r.method(http::Method::GET).h(graphiql))
+	}).bind("127.0.0.1:4010")
+	.unwrap()
+	.start();
+
+	println!("Started http server: 127.0.0.1:4010");
+	let _ = sys.run();
+}
