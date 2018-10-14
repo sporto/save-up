ab120118e59e6a186c681cd480db7621555f01b4
Sebastian Porto <s@porto5.com>
2018-10-14 17:01:49 +1100

    Get Authorization header

--- api-ocaml/bin/main.ml
+++ api-ocaml/bin/main.ml
@@ -1,6 +1,19 @@
 open Graphql_lwt
+open Cohttp
 open Lib
+open Core
+
+let get_context (req: Cohttp.Request.t) =
+	let
+		authHeader = 
+			req
+				|> Request.headers 
+				|> (Fn.flip Header.get) "Authorization"
+	in
+	match authHeader with
+	| None -> ()
+	| Some(_header) -> ()
 
 let () =
-  Server.start ~port:4010 ~ctx:(fun _req -> ()) Graph.schema
+  Server.start ~port:4010 ~ctx:get_context Graph.schema
     |> Lwt_main.run
