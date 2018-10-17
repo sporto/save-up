dc85e501e97e75940f8eb08e3841f367d995ba86
Sebastian Porto <s@porto5.com>
2018-10-17 19:25:56 +1100

    Calculate interest on the fly when asking for balance

--- api/src/actions/accounts/get_balance.rs
+++ api/src/actions/accounts/get_balance.rs
@@ -1,16 +1,32 @@
+use super::calculate_interest;
+use chrono::prelude::*;
 use diesel::pg::PgConnection;
 use failure::Error;
-use models::transaction::Transaction;
+use models::account::Account;
 use models::cents::Cents;
+use models::transaction::Transaction;
 
 pub fn call(conn: &PgConnection, account_id: i32) -> Result<i64, Error> {
-	let previous_transaction = Transaction::find_last_by_account_id(&conn, account_id);
+	let account = Account::find(&conn, account_id)?;
 
-	match previous_transaction {
-		Ok(t) => {
-			let Cents(cents) = t.balance;
-			Ok(cents)
-		},
+	let previous_transaction_result = Transaction::find_last_by_account_id(&conn, account_id);
+
+	match previous_transaction_result {
+		Ok(previous_transaction) => {
+			let now = Utc::now().naive_utc();
+
+			// Calculate the interest on the fly
+			let Cents(interest) = calculate_interest::call(
+				previous_transaction.balance,
+				&account.yearly_interest,
+				previous_transaction.created_at,
+				now,
+			)?;
+
+			let Cents(cents) = previous_transaction.balance;
+
+			Ok(cents + interest)
+		}
 		Err(_) => Ok(0),
 	}
 }
