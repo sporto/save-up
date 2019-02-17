f3aad7da3cf69fd06385021b5f11602f287ae623
Sebastian Porto <s@porto5.com>
2019-02-17 15:02:37 +1100

    Add SQL Alchemy

--- api-chalice/app.py
+++ api-chalice/app.py
@@ -1,21 +1,59 @@
 from chalice import Chalice
 import graphene
+from sqlalchemy import Column, Integer, Sequence, String, create_engine
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from graphene_sqlalchemy import SQLAlchemyObjectType
+import os
 
 app = Chalice(app_name='api')
 
 
-class User(graphene.ObjectType):
-    name = graphene.String()
+POSTGRES_CONNECTION_STRING = os.environ.get('DATABASE_URL')
 
-    def resolve_name(self, info):
-        return "Sam Sample"
+engine = create_engine(
+    POSTGRES_CONNECTION_STRING, convert_unicode=True
+)
+
+db_session = scoped_session(sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+    # id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('users_id_seq'), primary_key=True)
+    name = Column(String)
+
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db_session.commit()
+
+
+class User(SQLAlchemyObjectType):
+    class Meta:
+        model = UserModel
+        # only return specified fields
+        only_fields = ("name",)
 
 
 class Admin(graphene.ObjectType):
     investors = graphene.List(graphene.NonNull(User))
 
     def resolve_investors(self, info):
-        return []
+        query = User.get_query(info)
+        return query.all()
 
 
 class AppQuery(graphene.ObjectType):
