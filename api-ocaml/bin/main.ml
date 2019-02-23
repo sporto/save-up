bf7a2b034a271e64fabc8744835710e60eca2bfc
Sebastian Porto <s@porto5.com>
2019-02-23 22:15:02 +1100

    Allow multiple graphql endpoints

--- api-ocaml/bin/main.ml
+++ api-ocaml/bin/main.ml
@@ -24,27 +24,50 @@ let get_context (req: Cohttp.Request.t) =
 			| Ok(_data) -> ()
 			| Error(_) -> ()
 
+let make_context _ = ()
+
 module Graphql_cohttp_lwt =
 	Graphql_cohttp.Make (Graphql_lwt.Schema) (Cohttp_lwt_unix.IO) (Cohttp_lwt.Body)
+
+
+let callback _conn req body =
+	let req_path =
+		Cohttp.Request.uri req |> Uri.path 
+	in
+	let path_parts =
+		Str.(split (regexp "/") req_path)
+	in
+	match req.meth, path_parts with
+  	| `POST, ["app";"graphql"] ->
+		Graphql_cohttp_lwt.execute_request
+			Graph_app.schema
+			(make_context req)
+			req
+			body
+  	| _ ->
+	  	let response =
+		  	Cohttp.Response.make ~status:`Not_found ()
+		in
+    	let body =
+			Cohttp_lwt.Body.of_string "Not Found"
+		in
+    	Lwt.return (`Response (response, body))
 
 let () =
 	let on_exn = function
 		| Unix.Unix_error (error, func, arg) ->
 			Logs.warn (fun m ->
-				m  "Client connection error %s: %s(%S)"
-				(Unix.error_message error) func arg
-			)
+			m  "Client connection error %s: %s(%S)"
+			(Unix.error_message error) func arg
+		)
 		| exn ->
 			Logs.err (fun m -> m "Unhandled exception: %a" Fmt.exn exn)
-	in
-	let callback =
-  		Graphql_cohttp_lwt.make_callback (fun _req -> ()) Graph.schema 
 	in
 	let server =
 		Cohttp_lwt_unix.Server.make_response_action ~callback ()
 	in
 	let mode = 
-		`TCP (`Port 8080) 
+  		`TCP (`Port 8080)
 	in
   	Cohttp_lwt_unix.Server.create ~on_exn ~mode server
-  		|> Lwt_main.run
+		|> Lwt_main.run
