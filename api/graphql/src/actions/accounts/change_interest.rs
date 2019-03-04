a19ea4b53ea9e8ec04798d35be329e191375c4a3
Sebastian Porto <s@porto5.com>
2019-03-04 18:47:42 +1100

    Pay interest before changing the yearly interest

--- api/graphql/src/actions/accounts/change_interest.rs
+++ api/graphql/src/actions/accounts/change_interest.rs
@@ -2,8 +2,12 @@ use crate::models::{account::Account, schema as db};
 use bigdecimal::BigDecimal;
 use diesel::{self, pg::PgConnection, prelude::*};
 use failure::Error;
+use super::pay_interest;
 
 pub fn call(conn: &PgConnection, account_id: i32, interest: BigDecimal) -> Result<Account, Error> {
+	// Create an interest transaction with the current interest
+	pay_interest::call(&conn, account_id)?;
+
 	diesel::update(db::accounts::table.filter(db::accounts::id.eq(account_id)))
 		.set(db::accounts::yearly_interest.eq(interest))
 		.get_result(conn)
